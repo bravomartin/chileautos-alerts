47b647427da2f3466b000a9957909068504a0ee0
Martin Bravo <bravomartin@gmail.com>
2017-11-06 19:47:47 -0300

    a chileautos

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import _ from "lodash";
-import { safeLoad } from "js-yaml";
-import cheerio from "cheerio";
-import req from "request-promise-native";
+import _ from 'lodash';
+import { safeLoad } from 'js-yaml';
+import cheerio from 'cheerio';
+import req from 'request-promise-native';
 
-import redis from "then-redis";
+import redis from 'then-redis';
 let cache = redis.createClient(process.env.REDIS_URL);
 if (process.env.REDIS_FLUSH) cache.flushdb();
 
-import { sendEmail, makeEmail, reqOptions, parseMoney } from "./functions";
+import { sendEmail, makeEmail, reqOptions, parseMoney } from './functions';
 
 let reqs = [];
 let toEmail;
@@ -26,21 +26,37 @@ req(process.env.SOURCE_PATH)
           return cheerio.load(body);
         })
         .then($ => {
-          if ($(".redwarning_failover").text() == "Resultado no encontrado.") {
-            $(".listing_thumbs").empty();
-          }
           return $;
         })
         .then($ => {
-          let $ads = $(".listing_thumbs .ad"),
+          let $ads = $('.search-listings__items .listing-item'),
             ads = [];
           $ads.each((i, el) => {
+            const $el = $(el);
+
+            let imgurl = $el.find('.carousel-inner .item.active').attr('style');
+            if (imgurl) {
+              imgurl = imgurl.match(/url\((.+)\)/)[1];
+              imgurl = imgurl.replace(/\\0000/g, '%');
+              imgurl = decodeURIComponent(imgurl);
+            }
+            $el
+              .find('.listing-item__carousel')
+              .html(`<img src="${imgurl}" width="300"/>`);
+
+            const url = $el.find('.listing-item__header a').attr('href');
+
             let ad = {
-              id: $(el).attr("id").trim(),
-              html: $(el).html()
+              id: url.split('/').pop(),
+              url: url,
+              html: $el.html(),
+              title: $el.find('.listing-item__title').text(),
+              price: $el.find('.listing-item__price p').text(),
+              imgurl: imgurl
             };
             ads.push(ad);
           });
+          // return [];
           return ads;
         })
         .then(ads => {
@@ -62,10 +78,10 @@ req(process.env.SOURCE_PATH)
         })
         .then(group => {
           if (group.items.length < 1) {
-            console.log("nada nuevo para ", name);
+            console.log('nada nuevo para ', name);
             return null;
           } else {
-            console.log("%s nuevos para %s", group.items.length, name);
+            console.log('%s nuevos para %s', group.items.length, name);
             return group;
           }
         })
@@ -79,9 +95,10 @@ req(process.env.SOURCE_PATH)
       groups = _.compact(groups);
       if (groups.length > 0) {
         console.log(`enviando notificaciones a ${toEmail}`);
-        sendEmail(makeEmail(groups), toEmail, true);
+        const email = makeEmail(groups);
+        sendEmail(email, toEmail, true);
       } else {
-        console.log("nada que reportar");
+        console.log('nada que reportar');
         process.exit(0);
       }
     });
--- functions.js
+++ functions.js
@@ -1,48 +1,59 @@
-import mailcomposer from 'mailcomposer'
-import Mailgun from 'mailgun-js'
-import nunjucks from 'nunjucks'
+import fs from 'fs';
+import mailcomposer from 'mailcomposer';
+import Mailgun from 'mailgun-js';
+import nunjucks from 'nunjucks';
 nunjucks.configure('.', { autoescape: true });
-let mailgun = Mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
+let mailgun = Mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+});
 
 //REQ
-export let reqOptions = { headers: {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-  'Cache-Control': 'no-cache'
-}}
+export let reqOptions = {
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Cache-Control': 'no-cache'
+  }
+};
 
 //MONEY
-export function parseMoney(str){
-  return Number(str.replace(/[^0-9\,]+/g,""));
+export function parseMoney(str) {
+  return Number(str.replace(/[^0-9\,]+/g, ''));
 }
 
 // EMAIL
-export function makeEmail(groups){
-  return nunjucks.render('template.tmpl', {groups: groups})
+export function makeEmail(groups) {
+  return nunjucks.render('template.tmpl', { groups: groups });
 }
 
-export function sendEmail(email, toEmail, exit){
+export function sendEmail(email, toEmail, exit) {
   let mail = mailcomposer({
-    from: 'Yapo Alerts <alerts@sandboxdebf46a0d2c34fe390501cdd02ee004d.mailgun.org>',
+    from:
+      'Yapo Alerts <alerts@sandboxdebf46a0d2c34fe390501cdd02ee004d.mailgun.org>',
     to: toEmail,
-    subject: "Hay nuevas publicaciones en tus busquedas!",
+    subject: 'Hay nuevas publicaciones en tus busquedas!',
     body: 'Abrelo...',
     html: email,
     encoding: 'utf-8'
-  })
-
-  mail.build( function(mailBuildError, message){
+  });
+  if (process.env.FAKE) {
+    fs.writeFileSync('email.hmtl', email, { encoding: 'utf-8' });
+    if (exit) process.exit(0);
+  }
+  mail.build(function(mailBuildError, message) {
     let dataToSend = {
       to: toEmail,
       message: message.toString('ascii')
-    }
-    mailgun.messages().sendMime( dataToSend, (sendError, body)=>{
-      if (sendError){
+    };
+    mailgun.messages().sendMime(dataToSend, (sendError, body) => {
+      if (sendError) {
         console.error(sendError);
-        if(exit) process.exit(1)
+        if (exit) process.exit(1);
       } else {
-        console.log("Email sent!")
-        if(exit) process.exit(0)
+        console.log('Email sent!');
+        if (exit) process.exit(0);
       }
-    })
-  })
+    });
+  });
 }
