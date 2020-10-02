17a10fb68e807c0bcd21efe90d44732d43f0184d
Martin Bravo <bravomartin@gmail.com>
2020-10-02 18:26:14 -0300

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -10,20 +10,20 @@ const nunjucks = require('nunjucks');
 // config
 
 const octokit = new Octokit({
-	auth: process.env.TOKEN_GITHUB
+	auth: process.env.TOKEN_GITHUB,
 });
 
 nunjucks.configure('.', { autoescape: true });
 
 const mailgun = Mailgun({
 	apiKey: process.env.MAILGUN_API_KEY,
-	domain: process.env.MAILGUN_DOMAIN
+	domain: process.env.MAILGUN_DOMAIN,
 });
 
 //REQ
 const host = 'https://www.chileautos.cl';
 
-const curl = url => {
+const curl = (url) => {
 	return new Promise((resolve, reject) => {
 		let res = '';
 		const child = spawn(`curl`, [
@@ -52,12 +52,12 @@ const curl = url => {
 			'accept-language: en-US,en;q=0.9,es;q=0.8',
 			'-H',
 			'cookie: _fbp=fb.1.1579544057708.960918247; _ga=GA1.2.638796359.1579544058; gaclientId=638796359.1579544058; DeviceId=351d3535-f5b8-44f0-bd0b-485d63170394; .AspNetCore.Antiforgery.NsPtgLTHySs=CfDJ8NtsynzkTvhOqu6Mz85IBFMS5r9Hj4XQCHFcJPgbNaY4B_jTe_lOZjv9YEoF6y5gWysyaCL_7iPSJCJxyYnwUurw3-JQd35uJnyJkGv45-DLx-WNNxoRzw4Qg5lq3yW6Xhmf56bNI9V6RcQ7Ta9jRqk; _hjid=eb3427ee-eec5-41ca-9a3d-f916bd37447e; p_csnclientid=6cc0bdb6-c364-70c8-aba9-41a10748db81; csnclientid=a7d2ba09-2167-a5c8-beb3-97bea722f808-40f237a6-88da-4e74-b398-a74915149f97-1582743772012; cidgenerated=client; XSRF-TOKEN=CfDJ8NtsynzkTvhOqu6Mz85IBFO0fZbiUTSoHH2cpKn2sR2EMyuqefmIoO9bZtw73A1ip5qexhrIL1PHG_7ThYfcDqunRvPg1KfxtgOAxMntkD6KNQxsqtPty98tJZ_vjTqTaVTEEqCLDfqtuomGuKwvqYs; cmp.ret.enq.afgtoken=CfDJ8Efs2BAxx71EoBhTrHnI3sMbzHNmlzhXThIHDMyteK064lgf_MDQ_Cs6w_1hQ0iVoFhIvWc6nrUIwuAsIkZ4GfCy3j0ueCke2bmG-wIrNOvIPQNykpkkPXdkMG_vWzQlep9DdLI5qYZcjS1gAxF0hUc; _gid=GA1.2.2112131611.1587835278; csn.bi=1587923238759',
-			'--compressed'
+			'--compressed',
 		]);
-		child.stdout.on('data', data => {
+		child.stdout.on('data', (data) => {
 			res = res + data;
 		});
-		child.on('close', function(code) {
+		child.on('close', function (code) {
 			resolve(res.toString());
 		});
 	});
@@ -78,15 +78,15 @@ const getResults = async (url, results = []) => {
 					url: host + item.querySelector('h3 a').getAttribute('href'),
 					price: parseMoney(item.querySelector('.price a').textContent),
 					priceDisplay: item.querySelector('.price a').textContent.replace(/CLP\s*/g, ''),
-					img: item.querySelector('.carousel-item img').getAttribute('src')
+					img: item.querySelector('.carousel-item img').getAttribute('src'),
 				};
 
 				result.details = [];
 				item.querySelectorAll('.key-detail') &&
-					item.querySelectorAll('.key-detail').forEach(detail => {
+					item.querySelectorAll('.key-detail').forEach((detail) => {
 						result.details.push({
 							name: detail.querySelector('.key-detail-type').textContent,
-							value: detail.querySelector('.key-detail-value').textContent
+							value: detail.querySelector('.key-detail-value').textContent,
 						});
 					});
 				results.push(result);
@@ -117,21 +117,21 @@ function sendEmail(data, toEmail) {
 	console.log('Sending email...');
 	const email = nunjucks.render('template.tmpl', { data });
 	let mail = mailcomposer({
-		from: 'Chileautos Alerts <alerts@sandboxdebf46a0d2c34fe390501cdd02ee004d.mailgun.org>',
+		from: 'Chileautos Alerts <alerts@email.bravomartin.cl>',
 		to: toEmail,
 		subject: 'Hay nuevos autos en tus busquedas!',
 		body: 'Abrelo...',
 		html: email,
-		encoding: 'utf-8'
+		encoding: 'utf-8',
 	});
 	if (process.env.MOCK == 'true') {
 		fs.writeFileSync('data/email.html', email, { encoding: 'utf-8' });
 		console.log('saved to data/email.html');
 	} else {
-		mail.build(function(mailBuildError, message) {
+		mail.build(function (mailBuildError, message) {
 			let dataToSend = {
 				to: toEmail,
-				message: message.toString('ascii')
+				message: message.toString('ascii'),
 			};
 			mailgun.messages().sendMime(dataToSend, (sendError, body) => {
 				if (sendError) {
@@ -151,12 +151,12 @@ const getStore = async () => {
 		console.log('getting data from file system');
 		return {
 			state: JSON.parse(fs.readFileSync('./data/state.json', 'utf8')),
-			config: safeLoad(fs.readFileSync('./data/config.yml', 'utf8'))
+			config: safeLoad(fs.readFileSync('./data/config.yml', 'utf8')),
 		};
 	}
 
 	const gist = await octokit.gists.get({
-		gist_id: process.env.GIST_ID
+		gist_id: process.env.GIST_ID,
 	});
 
 	const store = {};
@@ -171,7 +171,7 @@ const getStore = async () => {
 	return store;
 };
 
-const saveState = async state => {
+const saveState = async (state) => {
 	const content = JSON.stringify(state);
 	if (process.env.NODE_ENV == 'development') {
 		fs.writeFileSync('./data/state.json', content, 'utf8');
@@ -181,9 +181,9 @@ const saveState = async state => {
 				gist_id: process.env.GIST_ID,
 				files: {
 					'state.json': {
-						content
-					}
-				}
+						content,
+					},
+				},
 			});
 		} catch (e) {
 			console.log('error saving gist');
